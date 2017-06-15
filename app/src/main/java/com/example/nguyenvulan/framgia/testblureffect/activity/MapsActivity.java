145ab73e0ad1789f2dd22f8bf2f067976978d8d7
Btnisme123 <vulan238@gmail.com>
2017-06-15 10:49:23 +0700

    marker click event

--- app/src/main/java/com/example/nguyenvulan/framgia/testblureffect/activity/MapsActivity.java
+++ app/src/main/java/com/example/nguyenvulan/framgia/testblureffect/activity/MapsActivity.java
@@ -9,19 +9,25 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private ViewPager mViewPager;
+    List<Marker> mMarkers = new ArrayList<>();
+    Marker mPreviousMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
             .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -33,22 +39,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney1 = new LatLng(-36, 151);
+        LatLng sydney2 = new LatLng(-39, 151);
+        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.icon)).position(sydney).title("Marker 1"));
+        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.icon)).position(sydney1).title("Marker 2"));
+        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.icon)).position(sydney2).title("Marker 3"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (mPreviousMarker != null) {
+            mPreviousMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.icon));
+        }
+        marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.location_marker));
+        mPreviousMarker = marker;
+        return false;
     }
 }
--- app/src/main/java/com/example/nguyenvulan/framgia/testblureffect/viewpager/MapPagerAdapter.java
+++ app/src/main/java/com/example/nguyenvulan/framgia/testblureffect/viewpager/MapPagerAdapter.java
@@ -1,27 +1,22 @@
 package com.example.nguyenvulan.framgia.testblureffect.viewpager;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
 
 
 /**
  * Created by FRAMGIA\nguyen.vu.lan on 14/06/2017.
  */
 
-public class MapPagerAdapter extends FragmentPagerAdapter {
-
-    public MapPagerAdapter(FragmentManager fm) {
-        super(fm);
-    }
-
-    @Override
-    public Fragment getItem(int position) {
-        return null;
-    }
+public class MapPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
         return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
     }
 }
